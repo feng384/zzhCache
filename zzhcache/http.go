3cfa8c0313b8c39739e25ebd3718e18f1deed433
ZZH <2358538953@qq.com>
2025-04-23 23:53:24 +0800

    add peers

--- zzhcache/http.go
+++ zzhcache/http.go
@@ -2,32 +2,44 @@ package zzhcache
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+	"zzhcache/consistenthash"
 )
 
-const defaultBasePath = "/_zzhcache/"
+const (
+	defaultBasePath = "/_zzhcache/"
+	defaultReplicas = 50
+)
 
-type HTTPPoll struct {
-	self     string
-	basePath string
+// HTTPPool implements PeerPicker for a pool of HTTP peers.
+type HTTPPool struct {
+	//this peer's base URL, e.g. "https://example.net:8000"
+	self       string
+	basePath   string
+	mu         sync.Mutex //guards peers and httpGetters
+	peers      *consistenthash.Map
+	httpGetter map[string]*httpGetter // keyed by e.g. "http://10.0.0.2:8008"
 }
 
-func NewHTTPPoll(self string) *HTTPPoll {
-	return &HTTPPoll{
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
 		self:     self,
 		basePath: defaultBasePath,
 	}
 }
 
-func (p *HTTPPoll) Log(format string, v ...interface{}) {
+func (p *HTTPPool) Log(format string, v ...interface{}) {
 	log.Printf("[Server %s] %s ", p.self, fmt.Sprintf(format, v...))
 }
 
-func (p *HTTPPoll) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
-		panic("HTTPPoll serving unexpected path: " + r.URL.Path)
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
 	// /<basePath>/<groupName>/<key> required
@@ -59,3 +71,59 @@ func (p *HTTPPoll) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+// Set updates the poll's list of peers.
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetter = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetter[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetter[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+
+type httpGetter struct {
+	baseURL string
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", resp.Status)
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read body: %v", err)
+	}
+
+	return bytes, nil
+
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
